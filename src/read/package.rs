63dfd1f14f16dd47d1d26212698bb849b25848d5
grtcdr <ba.tahaaziz@gmail.com>
2021-03-11 01:31:10 +0100

    fix: incorrect error message for a command's failure

--- src/read/package.rs
+++ src/read/package.rs
@@ -73,7 +73,7 @@ pub fn package_count(fail: &mut Fail) -> String {
             .wait_with_output()
             .expect("ERROR: failed to wait for \"wc\" process to exit");
         return String::from_utf8(output.stdout)
-            .expect("ERROR: \"dpkg -l | wc -l\" output was not valid UTF-8")
+            .expect("ERROR: \"ls /var/db/pkg | wc -l\" output was not valid UTF-8")
             .trim()
             .to_string();
     }
