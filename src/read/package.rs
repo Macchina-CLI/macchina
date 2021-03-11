9b6c20353521a84d504c8de42ccdc1a5b745de51
Scott Little <k1206150@students.katyisd.org>
2021-03-10 19:21:11 -0600

    add voidlinux support

--- src/read/package.rs
+++ src/read/package.rs
@@ -76,9 +76,32 @@ pub fn package_count(fail: &mut Fail) -> String {
             .expect("ERROR: \"dpkg -l | wc -l\" output was not valid UTF-8")
             .trim()
             .to_string();
-    }
-    fail.packages.fail_component();
-    return String::from("0");
+    } else if extra::which("xbps-query") {
+        let xbps = Command::new("xbps-query")
+        .arg("-l")
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("ERROR: failed to start \"xbps-query\" process");
+
+        let xbps_out = xbps.stdout.expect("ERROR: failed to open \"xbps-query\" stdout");
+
+        let count = Command::new("wc")
+            .arg("-l")
+            .stdin(Stdio::from(xbps_out))
+            .stdout(Stdio::piped())
+            .spawn()
+            .expect("ERROR: failed to start \"wc\" process");
+
+        let output = count
+            .wait_with_output()
+            .expect("ERROR: failed to wait for \"wc\" process to exit");
+        return String::from_utf8(output.stdout)
+            .expect("ERROR: \"xbps-query -l | wc -l\" output was not valid UTF-8")
+            .trim()
+            .to_string();
+        }
+        fail.packages.fail_component();
+        return String::from("0");
 }
 
 #[cfg(target_os = "netbsd")]
