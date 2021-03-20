d6c2ff7e3a5b91bd3899c66361664595c33c157f
Marvin Haschker <marvin@haschker.me>
2021-03-20 22:15:47 +0100

    ⚙️ Add basic linux and netbsd ascii but not yet platform specific

--- macchina/src/ascii.rs
+++ macchina/src/ascii.rs
@@ -43,10 +43,47 @@ WWWWWWWWWWWWWW  WWWWWWWWWWWWWW"#];
 
 #[cfg(target_os = "linux")]
 pub(crate) fn get_ascii_art() -> Box<&[&'static str]> {
-    //todo
+    const ASCII_ARRAY: &[&str] = &[r#"         _nnnn_
+        dGGGGMMb
+       @p~qp~~qMb
+       M|@||@) M|
+       @,----.JM|
+      JS^\__/  qKL
+     dZP        qKRb
+    dZP          qKKb
+   fZP            SMMb
+   HZM            MMMM
+   FqM            MMMM
+ __| ".        |\dS"qML
+ |    `.       | `' \Zq
+_)      \.___.,|     .'
+\____   )MMMMMP|   .'
+     `-'       `--'"#];
+
+    //todo add distribution specific art
+    Box::new(ASCII_ARRAY)
 }
 
 #[cfg(target_os = "netbsd")]
 pub(crate) fn get_ascii_art() -> Box<&[&'static str]> {
-    //todo
+    const ASCII_ARRAY: &[&str] = &[r#"                                 __,gnCCCOObaau
+       _._                  __,gnnCCCPF"''
+      (N\XCbngg,._____.,gnnndCCCCCCC___,
+       \N\XCCCCCCCCCCCCCCCCCCCCCCCCCCCCOOOOPYv
+        \N\XCCCCCCCCCCCCCCCCCCCCCCCCCCCCPF"''
+         \N\XCCCCCCCCCCCCCCCCCCCCOF"'
+          \N\XCCCCCCCCCCCCCCCCF"'
+           \N\XCCCCCCCCCCCCCF"'
+            \N\"PCOCCCOC"
+             \N\
+              \N\
+               \N\
+                \N\
+                 \N\
+                  \N\
+                   \N\
+                    \N\
+                     \N\"#];
+
+    Box::new(ASCII_ARRAY)
 }
