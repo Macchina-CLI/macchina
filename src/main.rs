10305e36df165088cd9053948b55abb2fc5f64b4
grtcdr <ba.tahaaziz@gmail.com>
2021-06-27 18:00:58 +0100

    Ignore bars when tts feature is enabled

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ mod theme;
 
 use cli::{MacchinaColor, Opt};
 use colored::Colorize;
+use tui::text::Span;
 use std::io;
 use std::path::Path;
 use structopt::StructOpt;
@@ -275,6 +276,34 @@ fn main() -> Result<(), io::Error> {
     let should_display = should_display(&opt);
     let theme = create_theme(&opt);
     let readout_data = data::get_all_readouts(&opt, &theme, should_display);
+
+    if cfg!(feature = "tts") { 
+        use google_speech::{Lang, Speech};
+
+        for readout in readout_data {
+            if let Ok(key) = Speech::new(readout.0.to_string(), Lang::en_us) {
+                if let Err(_speak) = key.play() {
+                    return Ok(());
+                }
+            }
+
+            if let Ok(lines) = readout.1 {
+                for line in lines {
+                    let vec = line.0;
+                    for val in vec {
+                        if let Ok(value) = Speech::new(val.content.to_string(), Lang::en_us) {
+                            if let Err(_speak) = value.play() {
+                                return Ok(());
+                            }
+                        }
+                    }
+                }
+                
+            }
+        }
+
+        return Ok(());
+    }
 
     if opt.doctor {
         doctor::print_doctor(&readout_data);
