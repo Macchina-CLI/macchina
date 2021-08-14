74724f4e4e5affaae820874a32d5ea2f2513c962
Uttarayan Mondal <uttarayan21@gmail.com>
2021-08-14 09:06:48 +0530

    Remove useless dependencies

--- build.rs
+++ build.rs
@@ -1,23 +1,6 @@
 #![allow(dead_code)]
 use vergen::{ShaKind,Config};
-// use structopt::clap::Shell;
-// mod format {
-//     include!("src/format.rs");
-// }
-// mod bars {
-//     include!("src/bars.rs");
-// }
-// mod data {
-//     include!("src/data/mod.rs");
-// }
-// mod theme {
-//     include!("src/theme.rs");
-// }
-// mod cli {
-//     include!("src/cli.rs");
-// }
 fn main() {
-    // let name = "macchina";
     let outdir = match std::env::var_os("OUT_DIR") {
         None => return,
         Some(outdir) => outdir,
@@ -28,15 +11,8 @@ fn main() {
         panic!("failed to write {}: {}", stamp_path.display(), err);
     }
 
-
-    // let mut cli = cli::build_cli();
-    // cli.gen_completions(name, Shell::Fish, &outdir);
-    // cli.gen_completions(name, Shell::Bash, &outdir);
-
-
     let mut config = Config::default();
     *config.git_mut().sha_kind_mut() = ShaKind::Short;
-    // Fix this later 
     // This will break builds if the git folder is removed
     vergen::vergen(config).unwrap(); 
 }
