e37bbceaf5d8507f5b6ac99651b0b95e0e6f3bfd
grtcdr <mail@grtcdr.rocks>
2021-09-13 05:34:18 +0100

    build.rs: remove unused code and account for vergen failure

--- build.rs
+++ build.rs
@@ -1,20 +1,15 @@
-#![allow(dead_code)]
+use std::env;
 use vergen::{Config, ShaKind};
+
 fn main() {
-    let outdir = match std::env::var_os("OUT_DIR") {
-        None => return,
-        Some(outdir) => outdir,
-    };
+    let target = env::var("TARGET").unwrap();
 
-    let stamp_path = std::path::Path::new(&outdir).join("macchina-stamp");
-    if let Err(err) = std::fs::File::create(&stamp_path) {
-        panic!("failed to write {}: {}", stamp_path.display(), err);
-    }
-
-    let mut config = Config::default();
-    *config.git_mut().sha_kind_mut() = ShaKind::Short;
-
-    if let Err(e) = vergen::vergen(config) {
-        eprintln!("{}", e)
+    if !target.contains("netbsd") {
+        let mut config = Config::default();
+        *config.git_mut().sha_kind_mut() = ShaKind::Short;
+    
+        if let Err(e) = vergen::vergen(config) {
+            eprintln!("{}", e)
+        }
     }
 }
