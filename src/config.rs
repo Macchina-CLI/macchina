af1b28073fe0b5c7ba1e33e5444ea3cead644a2f
grtcdr <ba.tahaaziz@gmail.com>
2021-10-24 01:46:19 +0100

    Patch opt with missing args

--- src/config.rs
+++ src/config.rs
@@ -14,17 +14,19 @@ impl Opt {
             if let Ok(mut file) = std::fs::File::open(path) {
                 let mut buffer: Vec<u8> = Vec::new();
                 if file.read_to_end(&mut buffer).is_ok() {
-                    toml::from_slice(&buffer).or(Err("Failed to parse config"))
+                    toml::from_slice(&buffer).or(Err("Failed to parse configuration file."))
                 } else {
-                    Err("Failed to read config file")
+                    Err("Failed to read configuration file.")
                 }
             } else {
-                Err("Failed to open file")
+                Err("Failed to open configuration file.")
             }
         } else {
-            Err("File doesn't exist")
+            Err("Failed to locate the file, perhaps it doesn't exist.")
         }
     }
+
+    /// Reads config file specified by MACCHINA_CONF environment variable
     pub fn from_config() -> Result<Opt, &'static str> {
         if let Some(path) = std::env::var_os("MACCHINA_CONF") {
             return Opt::from_config_file(&path);
@@ -46,42 +48,70 @@ impl Opt {
                 }
             }
         }
+
         Ok(Opt::default())
     }
 
+    /// Patches `opt` with the values provided in the command-line
     pub fn patch_args(&mut self, args: Self) {
         if args.version {
             self.version = true;
         }
+
         if args.doctor {
             self.doctor = true;
         }
+
         if args.export_config {
             self.export_config = true;
         }
-        if args.no_ascii {
-            self.no_ascii = true;
+
+        if args.current_shell {
+            self.current_shell = true;
         }
-        if args.palette.is_some() {
-            self.palette = args.palette;
-        }
+
         if args.long_shell {
             self.long_shell = true;
         }
-        if args.interface.is_some() {
-            self.interface = args.interface;
-        }
+
         if args.long_uptime {
             self.long_uptime = true;
         }
-        if args.show.is_some() {
-            self.show = args.show;
+
+        if args.list_themes {
+            self.list_themes = true;
         }
+
+        if args.long_kernel {
+            self.long_shell = true;
+        }
+
+        if args.no_ascii {
+            self.no_ascii = true;
+        }
+
+        if args.custom_ascii.is_some() {
+            self.custom_ascii = args.custom_ascii;
+        }
+
+        if args.config.is_some() {
+            self.config = args.config;
+        }
+
         if args.theme.is_some() {
             self.theme = args.theme;
         }
-        if args.list_themes {
-            self.list_themes = true;
+
+        if args.show.is_some() {
+            self.show = args.show;
+        }
+
+        if args.palette.is_some() {
+            self.palette = args.palette;
+        }
+
+        if args.interface.is_some() {
+            self.interface = args.interface;
         }
     }
 }
