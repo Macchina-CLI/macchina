a7562122648d88092ef6cd41bdb4886c1b367a21
Matthias Baer <matthiasbaer@bluewin.ch>
2021-12-19 09:57:31 +0100

    fix(windows): configuration sourcing (closes #242)

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,5 @@
 use crate::cli::{Opt, PKG_NAME};
 use crate::error::Result;
-use crate::extra;
 use std::io;
 use std::path::{Path, PathBuf};
 
@@ -18,7 +17,7 @@ pub fn read_config<S: AsRef<std::ffi::OsStr> + ?Sized>(path: &S) -> Result<Opt> 
 }
 
 pub fn get_config() -> Result<Opt> {
-    if let Some(mut path) = extra::config_dir() {
+    if let Some(mut path) = dirs::config_dir() {
         match cfg!(target_os = "macos") {
             true => {
                 if let Ok(home) = std::env::var("HOME") {
