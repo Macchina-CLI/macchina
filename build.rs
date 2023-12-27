1a4c1f541513c36301551967a667c29aa56caa64
grtcdr <tahaaziz.benali@esprit.tn>
2023-12-27 09:41:12 +0100

    Update build.rs environment emitting logic
    Fixes: #296

--- build.rs
+++ build.rs
@@ -1,15 +1,13 @@
 use std::env;
-use vergen::{Config, ShaKind};
+use std::error::Error;
+use vergen::EmitBuilder;
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let target = env::var("TARGET").unwrap();
 
     if !target.contains("netbsd") {
-        let mut config = Config::default();
-        *config.git_mut().sha_kind_mut() = ShaKind::Short;
-
-        if let Err(e) = vergen::vergen(config) {
-            eprintln!("{e}")
-        }
+        EmitBuilder::builder().emit()?;
     }
+
+    Ok(())
 }
